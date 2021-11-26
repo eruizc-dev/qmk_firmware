94c69fd6194fc1ea49f32bcb2a53d6273b962598
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-11-25 21:11:12 -0300

    Add my config

---
+++ keyboards/crkbd/keymaps/eruizc-dev/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// General
+#define MASTER_LEFT
+#define USE_SERIAL_PD2
+
+// Combo
+#define COMBO_COUNT 2
+
+// Oled
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+// Tapping
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 150
